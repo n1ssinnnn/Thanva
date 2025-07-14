c4f15d0ece159f6c8e373d3aefb4a3c0595f86c4
Natthanon Weerapong <nxnecrx@Natthanons-MacBook-Air.local>
2025-07-14 15:54:30 +0700

    organize

--- main.py
+++ main.py
@@ -3,33 +3,24 @@ import pytesseract
 import numpy as np
 import function as fn
 
-# Path
 answer_img_path = "answer_sheet/20250529110230_002.jpg"
 student_img_path = "answer_sheet/20250529110230_001.jpg"
 
-# โหลดภาพสี (ไว้ไฮไลท์)
 student_answer_color = cv2.imread(student_img_path)
 
-# อ่านคำตอบจาก user,correct
 from function import load_extract_anwers
 user_answers, correct_answers = load_extract_anwers(student_img_path, answer_img_path)
 
 print(user_answers)
 print(correct_answers)
  
-
-# STEP 3: ตรวจคำตอบ
 flags = fn.grade_answers(user_answers, correct_answers)
-# print("Flags:", flags)
 
-# STEP 4: Highlight
 final_img = fn.highlight_per_question_by_answer(student_answer_color, flags)
 
 score, results = fn.score_answers_by_group(user_answers, correct_answers)
 print("คะแนน:", score)
-# print("ผลแต่ละข้อ:", results)
 
-# แสดงผล
 cv2.imshow("Result", final_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
